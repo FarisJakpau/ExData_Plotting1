9f8971793babe4b561f603db5ffb14a360360f58
Salimah Mokhtar <salimah@um.edu.my>
2015-08-08 08:50:50 +0800

    latest plot4

--- plot4.R
+++ plot4.R
@@ -1,11 +1,12 @@
 ### PLOT 4 ###
 
-## Getting the entire dataset : Electric power consumption
-data_entire <- read.csv("/Users/salimahmokhtar/household_power_consumption.txt", header=TRUE, sep=';', na.strings="?",nrows=2075259, check.names=FALSE, stringsAsFactors=FALSE, comment.char="", quote='\"')
+library(data.table)
 
-data_entire$Date <- as.Date(data_entire$Date, format="%d/%m/%Y")
+## Getting the entire dataset : Electric power consumption
+data_entire <- fread("/Users/salimahmokhtar/household_power_consumption.txt", na.strings="?")
 
 ## Subsetting the data from the dates 2007-02-01 and 2007-02-02
+data_entire$Date <- as.Date(data_entire$Date, format="%d/%m/%Y")
 data <- subset(data_entire, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 rm(data_entire)
 
@@ -13,23 +14,22 @@ rm(data_entire)
 datetime <- paste(as.Date(data$Date), data$Time)
 data$Datetime <- as.POSIXct(datetime)
 
-## Plot 4
+#Output plot 4 to png
+png(filename="plot4.png", width=480, height=480)
+
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(data, {
-  plot(Global_active_power~Datetime, type="l", 
+  plot(Global_active_power ~ Datetime, type="l", 
        ylab="Global Active Power", xlab="")
-  plot(Voltage~Datetime, type="l", 
+  plot(Voltage ~ Datetime, type="l", 
        ylab="Voltage", xlab="datetime")
-  plot(Sub_metering_1~Datetime, type="l", 
-       ylab="Energy sub metering", xlab="")
-  lines(Sub_metering_2~Datetime,col='Red')
-  lines(Sub_metering_3~Datetime,col='Blue')
+  plot(Sub_metering_1 ~ Datetime, type="l", 
+       ylab="Energy Sub Meteringr", xlab="")
+  lines(Sub_metering_2 ~ Datetime, col='Red')
+  lines(Sub_metering_3 ~ Datetime, col='Blue')
   legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
-         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.75)
-  plot(Global_reactive_power~Datetime, type="l", 
-       ylab="Global_reactive_power",xlab="datetime")
-})
-
-## Saving plot 4 to file
-dev.copy(png, file="plot4.png", height=480, width=480)
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power ~ Datetime, type="l", 
+       ylab="Global_reactive_power",xlab="datetime") }
+)
 dev.off()
